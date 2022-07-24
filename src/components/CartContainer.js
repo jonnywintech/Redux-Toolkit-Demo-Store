097a2ea03307519b38cb48f6e38e8b31e98fe746
Nikola Stankovic <jonnywintech@gmail.com>
2022-07-24 20:22:25 +0200

    added modal

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem';
-import { clearCart } from '../features/cart/cartSlice'
+import { openModal } from '../features/modal/modalSlice';
 const CartContainer = () => {
     const dispatch = useDispatch();
 
@@ -36,7 +36,7 @@ const CartContainer = () => {
                 <div className="cart-total">
                   <h4>Total <span>{total.toFixed(2)}</span></h4>
                 </div>
-                <button className="btn clear-btn" onClick={()=> dispatch(clearCart())}>clear cart</button>
+                <button className="btn clear-btn" onClick={()=> dispatch(openModal())}>clear cart</button>
             </footer>
         </section>
   )
